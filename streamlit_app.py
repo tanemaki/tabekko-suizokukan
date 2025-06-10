4c1596f7723bae0632b6c3befd1549015d496a5f
d-prime <tanemaki@users.noreply.github.com>
2025-06-10 09:09:03 +0000

    アプリ全体を実装

--- streamlit_app.py
+++ streamlit_app.py
@@ -135,7 +135,6 @@ def render_page():
         tabekko_df,
     )
 
-    visualize_image_processor(image_preprocessor)
     estimated_result = estimator.estimate(image_preprocessor.standardized_image)
 
     columns[1].info(f"これはたぶん「{estimated_result['estimated_class_name']}」だよ！")
@@ -154,6 +153,123 @@ def render_page():
     ax.legend().remove()
     plt.gca().invert_yaxis()
     columns[1].pyplot(figure)
+
+    dataset_visualizer = DatasetVisualizer(dataset, tabekko_df)
+
+    top_class_ids = estimated_result["sorted_class_ids"][:5]
+
+    with st.expander("上位ランキングされた動物を覚えるときに使ったデータ"):
+        for selected_class_id in top_class_ids:
+            dataset_visualizer.visualize_three_images(class_id=selected_class_id)
+
+    with st.expander("画像の前処理"):
+        visualize_image_processor(image_preprocessor)
+
+    with st.expander("どう考えたのか見てみる"):
+        visualize_result(estimated_result, estimator)
+
+    with st.expander("モデルの頭の中（QUBO行列）を見てみる"):
+        columns = st.columns(2)
+        qubo = estimator.qubo
+        figure, ax = plt.subplots(figsize=(10, 10))
+        ax.imshow(qubo, cmap="bwr", clim=(-2, 2))
+        columns[0].pyplot(figure)
+
+    with st.expander("覚えた形をみてみよう"):
+        selected_class_id = st.selectbox(
+            "select data",
+            options=tabekko_df.index,
+            format_func=lambda i: f"{i}: {tabekko_df.loc[i]['japanese_name']}",
+        )
+
+        dataset_visualizer.visualize_three_images(class_id=selected_class_id)
+
+
+class DatasetVisualizer:
+    def __init__(self, dataset, tabekko_df):
+        self.dataset = dataset
+        self.images = dataset["images"]
+        self.labels = dataset["labels"]
+        self.unique_class_ids = np.unique(self.labels)
+        self.files = dataset["files"]
+
+        self.tabekko_df = tabekko_df
+
+    def visualize_three_images(self, class_id):
+        figure, axs = plt.subplots(1, 3, figsize=(4, 1))
+
+        indices = np.where(self.labels == class_id)[0]
+        selected_indices = indices[:3]
+
+        selected_images = self.images[selected_indices]
+        selected_labels = self.labels[selected_indices]
+
+        figure, axs = plt.subplots(1, 3, figsize=(12, 2))
+
+        japanese_name = self.tabekko_df.loc[class_id]["japanese_name"]
+
+        ax = axs[0]
+        ax.imshow(selected_images[0], cmap="gray")
+        ax.set_title(f"Class ID: {selected_labels[0]} ({japanese_name})")
+        ax.axis("off")
+        ax = axs[1]
+        ax.imshow(selected_images[1], cmap="gray")
+        ax.set_title(f"Class ID: {selected_labels[1]} ({japanese_name})")
+        ax.axis("off")
+        ax = axs[2]
+        ax.imshow(selected_images[2], cmap="gray")
+        ax.set_title(f"Class ID: {selected_labels[2]} ({japanese_name})")
+        ax.axis("off")
+
+        st.pyplot(figure)
+
+
+def visualize_result(estimated_result, estimator):
+    figure, axs = plt.subplots(1, 3, figsize=(22, 6))
+
+    ax = axs[0]
+    ax.imshow(estimated_result["binary_vector"].reshape(28, 28))
+    ax.axis("off")
+
+    estimated_class_id = estimated_result["estimated_class_id"]
+    estimated_class_name = estimated_result["estimated_class_name"]
+
+    energies = estimated_result["energies"]
+    C = estimator.C
+
+    ax = axs[1]
+    ax.bar(np.arange(C), energies)
+    ax.axhline(np.min(energies), color="r", linestyle="--")
+    ax.set_title(f"Lowest energy label: {estimated_class_id} ({estimated_class_name})")
+    ax.set_ylabel("Energy")
+    ax.set_xticks(np.arange(C))
+    ax.set_xticklabels(estimator.xtick_labels, rotation=270)
+    max_energy = np.max(energies)
+    min_energy = np.min(energies)
+    energy_delta = max_energy - min_energy
+    ax.set_ylim(min_energy - 0.1 * energy_delta, max_energy + 0.1 * energy_delta)
+
+    probabilities = estimated_result["probabilities"]
+
+    highest_probability_class_index = np.argmax(probabilities)
+    highest_probability_class_id = estimator.unique_class_ids[
+        highest_probability_class_index
+    ]
+    ax = axs[2]
+    ax.bar(np.arange(len(probabilities)), probabilities)
+    ax.set_title(
+        f"Highest probability label: {highest_probability_class_id} (beta: {estimator.beta})"
+    )
+    ax.set_ylabel("Probability")
+    ax.set_xticks(np.arange(C))
+    ax.set_xticklabels(estimator.xtick_labels, rotation=270)
+    ax.axhline(np.max(probabilities), color="r", linestyle="--")
+
+    st.pyplot(figure)
+
+
+def get_class_index(class_id, unique_class_ids):
+    return int(np.where(unique_class_ids == class_id)[0][0])
 
 
 if __name__ == "__main__":
